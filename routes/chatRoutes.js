0af7b4b976edaa91a6d1ae58cd66a588fd2a9d74
Bryan Garcia <brayangarcia224@gmail.com>
2026-08-29 23:01:52 -0500

    Inicio del proyecto M3 - estructura de carpetas y archivos base

---
+++ routes/chatRoutes.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const router = Router()
+
+router.get('/',(req, res)=>{
+    res.json({mensaje: 'Lista de mensajes', exito: true})
+})
+
+router.post('/', (req,res)=>{
+    const {texto} = req.body
+    res.json({
+        mensaje: 'mensaje recibido: "${texto}"',
+        exito: true,
+        fecha: new Date()
+    })
+})
+
+module.exports = router
+
